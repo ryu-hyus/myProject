a0b16ed572740db6f07292aabd48151ec8da14a0
ryu-hyus <gustmd1128@naver.com>
2023-07-06 11:12:16 +0900

    hi

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from myApp.views import PersonView
+from myApp.views import HelloTemplateView
 
 app_name = "myApp"
 
 urlpatterns = [
     path('member/', PersonView.as_view(), name='member_list_create'),
     path('member/<int:pk>/', PersonView.as_view(), name='member_detail_update_delete'),
+    path("hello/", HelloTemplateView.as_view(), name='hello_template_view')
 ]
